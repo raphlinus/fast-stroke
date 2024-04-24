3e33bdc2fa21e40c329b749dbb233da4d71ba260
Raph Levien <raph@google.com>
2024-04-23 21:04:28 -0700

    More balance experiments
    Add a struct for balanced beziers, move away from ratio.

---
+++ src/balanced.rs
@@ -0,0 +1,73 @@
+use kurbo::CubicBez;
+
+#[derive(Debug)]
+pub struct BalancedBez {
+    pub th0: f64,
+    pub th1: f64,
+    // these are normalized, but un-normalized could be interesting
+    pub bulge: f64,
+    pub balance: f64,
+}
+
+impl BalancedBez {
+    pub fn to_cubic(&self) -> CubicBez {
+        let (s0, c0) = self.th0.sin_cos();
+        let (s1, c1) = self.th1.sin_cos();
+        let e0 = 2. / (3. + 3. * c0);
+        let e1 = 2. / (3. + 3. * c1);
+        let s01 = s0 * c1 + s1 * c0;
+        let b0 = 2. * s0 - e1 * s01;
+        let b1 = 2. * s1 - e0 * s01;
+        let norm = 1. / b0.hypot(b1);
+        let b0 = b0 * norm;
+        let b1 = b1 * norm;
+        let d0 = e0 + self.bulge * b0 + self.balance * b1;
+        let d1 = e1 + self.bulge * b1 - self.balance * b0;
+        //println!("d0 = {d0} d1 = {d1}");
+        CubicBez::new(
+            (0., 0.),
+            (d0 * c0, d0 * s0),
+            (1. - d1 * c1, d1 * s1),
+            (1., 0.)
+        )
+    }
+
+    #[allow(unused)]
+    pub fn from_cubic(c: CubicBez) -> Self {
+        let d01 = c.p1 - c.p0;
+        let d23 = c.p3 - c.p2;
+        let th0 = d01.atan2();
+        let th1 = -d23.atan2();
+        let l01 = d01.hypot();
+        let l23 = d23.hypot();
+        let (s0, c0) = (d01.y / l01, d01.x / l01);
+        let (s1, c1) = (-d23.y / l23, d23.x / l23);
+        let e0 = 2. / (3. + 3. * c0);
+        let e1 = 2. / (3. + 3. * c1);
+        let s01 = s0 * c1 + s1 * c0;
+        let b0 = 2. * s0 - e1 * s01;
+        let b1 = 2. * s1 - e0 * s01;
+        let norm = 1. / b0.hypot(b1);
+        let b0 = b0 * norm;
+        let b1 = b1 * norm;
+        let bulge = (l01 - e0) * b0 + (l23 - e1) * b1;
+        let balance = (l01 - e0) * b1 - (l23 - e1) * b0;
+        BalancedBez { th0, th1, bulge, balance }
+    }
+
+    pub fn get_ds(&self) -> (f64, f64) {
+        let (s0, c0) = self.th0.sin_cos();
+        let (s1, c1) = self.th1.sin_cos();
+        let e0 = 2. / (3. + 3. * c0);
+        let e1 = 2. / (3. + 3. * c1);
+        let s01 = s0 * c1 + s1 * c0;
+        let b0 = 2. * s0 - e1 * s01;
+        let b1 = 2. * s1 - e0 * s01;
+        let norm = 1. / b0.hypot(b1);
+        let b0 = b0 * norm;
+        let b1 = b1 * norm;
+        let d0 = e0 + self.bulge * b0 + self.balance * b1;
+        let d1 = e1 + self.bulge * b1 - self.balance * b0;
+        (d0, d1)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod balanced;
+
+use balanced::BalancedBez;
 use kurbo::{
     common::solve_quadratic, offset::CubicOffset, Affine, CubicBez, ParamCurve, ParamCurveArclen,
     ParamCurveFit,
@@ -78,7 +81,7 @@ impl NormOffset {
     ///
     /// In this case, balance is d0 / (d0 + d1), so meaningfully ranges from 0
     /// to 1. The returned Bézier is C1 continuous.
-    fn approx_from_balance(&self, balance: f64) -> CubicBez {
+    fn approx_from_balance_ratio(&self, balance: f64) -> CubicBez {
         let s0 = self.th0.sin();
         let s1 = self.th1.sin();
         let s01 = (self.th0 + self.th1).sin();
@@ -95,6 +98,7 @@ impl NormOffset {
         }
         let d0 = d * balance;
         let d1 = d * (1.0 - balance);
+        //println!("d0 = {d0} d1 = {d1}");
         CubicBez::new(
             (0., 0.),
             (d0 * self.th0.cos(), d0 * s0),
@@ -111,34 +115,88 @@ impl NormOffset {
             _ => cubic_default,
         }
     }
+
+    /// Approximate using sweet Bézier.
+    #[allow(unused)]
+    fn approx_sweet(&self) -> CubicBez {
+        let (s0, c0) = self.th0.sin_cos();
+        let (s1, c1) = self.th1.sin_cos();
+        let e0 = 2. / (3. + 3. * c0);
+        let e1 = 2. / (3. + 3. * c1);
+        let s01 = s0 * c1 + s1 * c0;
+        let b0 = 2. * s0 - e1 * s01;
+        let b1 = 2. * s1 - e0 * s01;
+        // Maybe normalize here, or not necessary?
+        let a0 = 0.15 * (2. * e0 * s0 + 2. * e1 * s1 - e0 * e1 * s01) - self.area;
+        let a1 = 0.15 * (2. * b0 * s0 + 2. * b1 * s1 - (e0 * b1 + e1 * b0) * s01);
+        let a2 = -0.15 * (b0 * b1 * s01);
+        let roots = solve_quadratic(a0, a1, a2);
+        //println!("{a2} u^2 + {a1} u + {a0}");
+        //println!("roots: {roots:?}");
+        let mut root = roots[0];
+        if roots.len() > 1 && roots[1].abs() < root.abs() {
+            root = roots[1];
+        }
+        let d0 = e0 + root * b0;
+        let d1 = e1 + root * b1;
+        //println!("d0 = {d0} d1 = {d1}");
+        CubicBez::new(
+            (0., 0.),
+            (d0 * c0, d0 * s0),
+            (1. - d1 * c1, d1 * s1),
+            (1., 0.)
+        )
+    }
+
+    /// Approximate finding area fit given starter d params.
+    fn approx_from_ds(&self, e0: f64, e1: f64) -> CubicBez {
+        let (s0, c0) = self.th0.sin_cos();
+        let (s1, c1) = self.th1.sin_cos();
+        let s01 = s0 * c1 + s1 * c0;
+        let b0 = 2. * s0 - e1 * s01;
+        let b1 = 2. * s1 - e0 * s01;
+        // Maybe normalize here, or not necessary?
+        let a0 = 0.15 * (2. * e0 * s0 + 2. * e1 * s1 - e0 * e1 * s01) - self.area;
+        let a1 = 0.15 * (2. * b0 * s0 + 2. * b1 * s1 - (e0 * b1 + e1 * b0) * s01);
+        let a2 = -0.15 * (b0 * b1 * s01);
+        let roots = solve_quadratic(a0, a1, a2);
+        let mut root = roots[0];
+        if roots.len() > 1 && roots[1].abs() < root.abs() {
+            root = roots[1];
+        }
+        let d0 = e0 + root * b0;
+        let d1 = e1 + root * b1;
+        CubicBez::new(
+            (0., 0.),
+            (d0 * c0, d0 * s0),
+            (1. - d1 * c1, d1 * s1),
+            (1., 0.)
+        )
+    }
 }
 
-fn balance_of_cubic(c: CubicBez) -> f64 {
+fn balance_ratio_of_cubic(c: CubicBez) -> f64 {
     let d0 = (c.p1 - c.p0).length();
     let d1 = (c.p3 - c.p2).length();
     d0 / (d0 + d1)
 }
 
 fn err_plot_main() {
-    let th0: f64 = 0.5;
-    let th1: f64 = 0.15;
-    let d0_scale = 1.0;
-    let d1_scale = 1.0;
-    let d0 = 2. / 3. * d0_scale / (1.0 + th0.cos());
-    let d1 = 2. / 3. * d1_scale / (1.0 + th1.cos());
-    let c = CubicBez::new(
-        (0.0, 0.0),
-        (d0 * th0.cos(), d0 * th0.sin()),
-        (1.0 - d1 * th1.cos(), d1 * th1.sin()),
-        (1.0, 0.0),
-    );
+    let bb = BalancedBez {
+        th0: 0.2,
+        th1: 0.3,
+        bulge: 0.3,
+        balance: 0.0,
+    };
+    let c = bb.to_cubic();
     const DIFF_EPS: f64 = 0.001;
     let no_p = NormOffset::new(c, DIFF_EPS);
-    let bal_p = balance_of_cubic(no_p.cubic_fit());
+    let bal_p = balance_ratio_of_cubic(no_p.cubic_fit());
     let no_m = NormOffset::new(c, -DIFF_EPS);
-    let bal_m = balance_of_cubic(no_m.cubic_fit());
+    let bal_m = balance_ratio_of_cubic(no_m.cubic_fit());
     let slope = (bal_p - bal_m) / (2. * DIFF_EPS);
-    let predict = 0.75 * (th0 - th1);
+    // Not a very good prediction; iterate
+    let predict = 0.375 * (bb.th0.abs() - bb.th1.abs());
     eprintln!("slope = {slope}, predict {predict}");
 
     const HEIGHT: usize = 256;
@@ -149,10 +207,17 @@ fn err_plot_main() {
     for y in 0..HEIGHT {
         let d = y as f64 / (HEIGHT - 1) as f64 - 0.5;
         let no = NormOffset::new(c, d);
-        let predict_bal = d0 / (d0 + d1) + d * predict;
+        let predict_bal = bb.balance + d * predict;
         for x in 0..WIDTH {
-            let balance = x as f64 / (WIDTH - 1) as f64;
-            let c = no.approx_from_balance(balance);
+            let balance = x as f64 / (WIDTH - 1) as f64 - 0.5;
+            let bb = BalancedBez {
+                th0: no.th0,
+                th1: no.th1,
+                bulge: 0.0,
+                balance,
+            };
+            let (d0, d1) = bb.get_ds();
+            let c = no.approx_from_ds(d0, d1);
             let err = no.cubic_err(c);
             let z = -20. * err.ln() - 50.0;
             let g = z.clamp(0.0, 255.0) as u8;
@@ -183,7 +248,7 @@ fn junk_main() {
         no.co_normed.sample_pt_deriv(0.5).1.hypot()
     );
     println!("err = {}", no.cubic_err(c));
-    println!("{:?}", no.approx_from_balance(0.46725));
+    println!("{:?}", no.approx_from_balance_ratio(0.46725));
 }
 
 fn main() {
